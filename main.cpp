8fba3124a97282a29e0ad58330ee313ba4bfba49
Ruslan <damin4@mail.ru>
2023-08-02 20:14:38 +0300

    c

--- main.cpp
+++ main.cpp
@@ -4,18 +4,12 @@
 
 int main() {
     std::cout << "Hello, Worl234d!" << std::endl;
-//    HMODULE hLib = LoadLibrary("RfSpringVisionSystemLib.dll");
-//
-//    if (hLib == NULL) {
-//        std::wcerr << "Library MyLibrary.dll not found" << std::endl;
-//        exit(1);
-//    }
-    //    bool status;
+    bool status;
     status = lib_init();
     std::cout<<status;
-        lib_clear();
-    lib_initialized();
-    move_to(123,329);
+    //    lib_clear();
+//    lib_initialized();
+//    move_to(123,329);
     return 0;
 }
 
