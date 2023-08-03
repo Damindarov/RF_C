4dabc1973ed7e26a56669dd79e5d273cf4fccf05
Ruslan <damin4@mail.ru>
2023-08-03 11:25:20 +0300

    Initial working version

--- main.cpp
+++ main.cpp
@@ -8,24 +8,6 @@ int main() {
     bool status;
     status = lib_init();
     std::cout<<status;
-    //    lib_clear();
-//    lib_initialized();
-//    move_to(123,329);
+
     return 0;
 }
-
-//#include <iostream>
-//#include <windows.h>
-//int main() {
-
-//    HINSTANCE hDll = LoadLibrary("RfSpringVisionSystemLib.dll");
-//    if (hDll != NULL)
-//
-//    {
-//
-//        printf("Library was loaded\n");
-//
-//    }
-//    std::cout << "Hello, Worl234d!" << std::endl;
-//    return 0;
-//}
